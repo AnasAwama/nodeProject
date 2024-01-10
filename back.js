ef9bd2c97ee65a3ca6767573367f22120e6ecb14
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2024-01-11 00:24:14 +0300

    Update back.js

--- back.js
+++ back.js
@@ -8,11 +8,42 @@ var urlEncodded=body.urlencoded({extended:false})
 
 app.use(express.static('public'))
 
+app.get("/retrieveMain",function(req,res){
 
+})
+
+
+app.get("/retrieveForm",function(req,res){
+    
+})
+
+
+app.get("/getAllProduct",function(req,res){
+    
+})
+
+
+app.post("/requestProduct/:id",function(req,res){
+    
+    var prod=myfs.readFileSync('product.json')
+
+    prod = JSON.parse(prod)
+    if(req.params.id>0&&req.params.id<=6){
+        res.send((users['user'+req.params.id]))
+    }
+    else{
+        res.send("No user with the following Id: "+req.params.id)
+    }
+})
+
+
+app.post("/addProduct",function(req,res){
+    
+})
 
 app.get("/",function(req,res){
 
-    res.send(__dirname+"/index.html");
+    res.send(__dirname+"/main.html");
 })
 
 var server=app.listen(8081,function(){
