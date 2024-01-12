f23e63d207bc9b73b7598ff35bcbed4099f60d71
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2024-01-12 13:58:39 +0300

    Update back.js

--- back.js
+++ back.js
@@ -4,49 +4,58 @@ var http=require('http');
 var myfs= require('fs')
 
 var body=require('body-parser')
-var urlEncodded=body.urlencoded({extended:false})
+app.use(body.json());
+var urlEncoded=body.urlencoded({extended:false})
+
+var prod=myfs.readFileSync('product.json')
 
 app.use(express.static('public'))
 
 app.get("/retrieveMain",function(req,res){
 
+    res.sendFile(__dirname + '/public/main.html');
 })
 
 
 app.get("/retrieveForm",function(req,res){
-    
+    res.sendFile(__dirname + '/public/request.html');
 })
 
 
 app.get("/getAllProduct",function(req,res){
-    
+
+    res.send(prod.toString())
 })
 
 
-app.post("/requestProduct/:id",function(req,res){
-    
-    var prod=myfs.readFileSync('product.json')
-
+app.post("/requestProduct/:id",urlEncoded,function(req,res){
     prod = JSON.parse(prod)
-    if(req.params.id>0&&req.params.id<=6){
-        res.send((users['user'+req.params.id]))
+    var prodId = req.params.id
+    if(prodId>0&&prodId<=6){
+        res.send((prod['prod'+prodId]))
     }
     else{
-        res.send("No user with the following Id: "+req.params.id)
+        res.send("No user with the following Id: "+prodId)
     }
 })
 
-
-app.post("/addProduct",function(req,res){
+app.post("/addProduct",urlEncoded,function(req,res){
     
+    var newProduct = {
+        id: req.body.id,
+        name: req.body.name,
+        category: req.body.category,
+        description: req.body.description
+    };
+    // prod['prod'+newProduct.id]=newProduct
+    // console.log("Received POST request to /addProduct",  prod.toString());
+    // var prods = req.body
+    myfs.writeFileSync('product.json',prod);
+    res.send(newProduct);
 })
 
-app.get("/",function(req,res){
 
-    res.send(__dirname+"/main.html");
-})
-
-var server=app.listen(8081,function(){
+var server=app.listen(8082,function(){
     var host = server.address().address
     var port= server.address().port
     console.log("working",host,port)
